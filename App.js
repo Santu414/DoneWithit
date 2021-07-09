06ffa252fa8db921d0d472982d0328d4921d8b90
Santosh Reddy <snagankera@gmail.com>
2021-07-09 10:26:54 +0530

    Practiced with icons

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>I am Santosh Reddy</AppText>
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
     </View>
   );
 }
