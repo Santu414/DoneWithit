bd849428fa4070305bfc773c5b35a5bd350a8ce8
Santosh Reddy <snagankera@gmail.com>
2021-07-09 10:11:54 +0530

    Added separate component for text

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, Button, SafeAreaView, Alert } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { Text, View } from "react-native";
 
+import AppText from "./app/components/AppText";
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>I am Santosh Reddy</AppText>
+    </View>
+  );
 }
