ef8ec482a3ab8d05e9e3803409d992fdc6d703fb
Santosh Reddy <snagankera@gmail.com>
2021-07-09 16:16:23 +0530

    Building Listing Details and List Item

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import { View } from "react-native";
+
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
-import AppText from "./app/components/AppText";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 export default function App() {
-  return (
-    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-      <Card
-        title="This is first"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
