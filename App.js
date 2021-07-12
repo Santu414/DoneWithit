56203d9add7426fbe77558958e633ee3d7f95eb1
Santosh Reddy <snagankera@gmail.com>
2021-07-12 19:00:06 +0530

    Biult the Login Screen

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
 import React, { useState } from "react";
+import LoginScreen from "./app/screens/LoginScreen";
 
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import Screen from "./app/components/Screen";
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
 export default function App() {
-  const [category, setCategory] = useState();
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
