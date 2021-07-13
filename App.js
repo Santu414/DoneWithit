27335a24b1d94ae7c706188827805bfea48c0fce
Santosh Reddy <snagankera@gmail.com>
2021-07-13 22:01:59 +0530

    Added Chevrons and Handling long text

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <RegisterScreen />;
+  return <ListingDetailsScreen />;
 }
