224791eaf2986b27060a38f50b885a7abc6b6a12
Santosh Reddy <snagankera@gmail.com>
2021-07-13 09:20:48 +0530

    organized form related components

---
+++ app/components/forms/index.js
@@ -0,0 +1,6 @@
+import AppForm from "./AppForm";
+import AppFormField from "./AppFormField";
+import ErrorMessages from "./ErrorMessages";
+import SubmitButton from "./SubmitButton";
+
+export { AppForm, AppFormField, ErrorMessages, SubmitButton };
