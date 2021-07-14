279528bb357b3129e7079b8d098bb114b490a50a
Santosh Reddy <snagankera@gmail.com>
2021-07-14 16:06:02 +0530

    Installed expo-permissions and worked on it

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-
+import * as Permissions from "expo-permissions";
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const requsetPermission = async () => {
+    const result = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL,
+      Permissions.LOCATION
+    );
     const { granted } = await ImagePicker.requestCameraRollPermissionsAsync;
     if (!granted) {
       alert("You need to enable the access to the library");
