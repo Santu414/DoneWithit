d692fcc5ba6ff07c0da5e1ea8bbeb874a3f0f0f6
Santosh Reddy <snagankera@gmail.com>
2021-07-14 17:00:36 +0530

    Worked on Accessing the library

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
   const requsetPermission = async () => {
-    const result = await Permissions.askAsync(
-      Permissions.CAMERA_ROLL,
-      Permissions.LOCATION
-    );
     const { granted } = await ImagePicker.requestCameraRollPermissionsAsync;
     if (!granted) {
       alert("You need to enable the access to the library");
@@ -17,5 +15,18 @@ export default function App() {
   useEffect(() => {
     requsetPermission();
   }, []);
-  return <Screen></Screen>;
+  selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error Reading an Image", error);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
