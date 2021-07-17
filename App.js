b6495efb2cd5f50508b8a9b076e421ccf6973016
Santosh Reddy <snagankera@gmail.com>
2021-07-18 04:04:37 +0530

    Installed expo AsyncStorage and worked on it

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
-import { View } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const netInfo = useNetInfo();
-
-  return netInfo.isConnected ? <View></View> : <View></View>;
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  demo();
+  return null;
 }
