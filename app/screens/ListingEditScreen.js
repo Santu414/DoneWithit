ebc6ccc46584692b31860884df6920972247516e
Santosh Reddy <snagankera@gmail.com>
2021-07-17 16:27:04 +0530

    Built the Upload Screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -15,6 +15,7 @@ import FormImagePicker from "../components/forms/FormImagePicker";
 import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
 import listings from "../api/listings";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -83,13 +84,23 @@ const categories = [
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
+
   const handleSubmit = async (listing) => {
-    const result = await listingsApi.addListings({ ...listing, location });
+    setUploadVisible(true);
+    const result = await listingsApi.addListings(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+    setUploadVisible(false);
+
     if (!result.ok) return alert("Cound not save the listings");
     alert("Success");
   };
   return (
     <Screen style={styles.container}>
+      <UploadScreen progress={progress} visible={uploadVisible} />
       <Form
         initialValues={{
           title: "",
