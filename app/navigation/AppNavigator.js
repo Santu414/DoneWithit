0d9c49fe6e9baadddc56da063740301f089fc5c4
Santosh Reddy <snagankera@gmail.com>
2021-07-20 02:57:57 +0530

    Added some modification to push notification code

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Notifications } from "expo-notifications";
+import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
 
 import AccountNavigator from "./AccountNavigator";
